a16546967832d1d971a60eb80532f57c4b1ac8cb
LimonkaTM <etuzin@mail.ru>
2022-04-22 20:49:04 +0300

    added files

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const contents = document.querySelectorAll('.program-line__content');
+
+// функция для закрытия всех табов аккордеона
+const disableActiveElems = () => {
+  contents.forEach( elem => {
+    elem.querySelector('.program-line__descr').classList.remove('active');
+  })
+};
+
+contents.forEach( elem => {
+  const title = elem.querySelector('.program-line__title');
+  const descr = elem.querySelector('.program-line__descr');
+
+  title.addEventListener('click', () => {
+    // закрываем все табы аккордеона
+    disableActiveElems()
+    
+    descr.classList.toggle('active')
+  })
+});
